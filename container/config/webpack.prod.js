f16f0bdf1b144987db9497108656c80a5d20b99e
radhaui <radhaui1612@gmail.com>
2024-11-11 19:07:47 +0530

    updated wepack config prod file

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -1,7 +1,7 @@
 const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
-const packageJson = require('../package.json');
+const dependencies = require("../package.json").dependencies;
 
 const domain = process.env.PRODUCTION_DOMAIN;
 
