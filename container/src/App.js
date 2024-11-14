202d20e60630fee0198b92186fff837a2f142b48
radhaui <radhaui1612@gmail.com>
2024-11-14 15:57:53 +0530

    removed apps from container

--- container/src/App.js
+++ container/src/App.js
@@ -1,15 +1,15 @@
 import React, { Suspense } from 'react';
 import './App.css';
-const App1 = React.lazy(()=>import("app1/main"));
-const App2 = React.lazy(()=>import("app2/main"));
+// const App1 = React.lazy(()=>import("app1/main"));
+// const App2 = React.lazy(()=>import("app2/main"));
 import Header from './components/Header';
 function App() {
   return (
     <>
     <Suspense fallback={<div>Loading...</div>}>
       <Header/>
-      <App1 />
-      <App2 />
+      {/* <App1 />
+      <App2 /> */}
     </Suspense>
   </>
   );
